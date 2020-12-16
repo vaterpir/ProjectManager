9fd929903c8874fa5cdd41e87ae75620c07b0d4b
vaterpir <vatepri@yandex.ru>
2020-12-16 19:41:56 +0300

    add action and constants /fix

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,24 @@
-import { ADD_BOARD } from '../constants';
+import {
+  ADD_BOARD,
+  CHANGE_NAME_BOARD,
+  DELETE_BOARD,
+  ADD_CARD,
+  CHANGE_NAME_CARD,
+  CHANGE_DESCRIPTION_CARD,
+  DELETE_CARD,
+  ADD_CHECKLIST,
+  CHANGE_NAME_CHECKLIST,
+  DELETE_CHECKLIST,
+  ADD_TASK,
+  CHANGEE_STATUS_TASK,
+  CHANGEE_NAME_TASK,
+  DELETE_TASK,
+  ADD_LABEL_TO_SEARCH,
+  DELETE_LABEL_TO_SEARCH,
+  CHANGE_TEXT_TO_SEARCH,
+  CREATE_COLOR_LABEL,
+  DELETE_COLOR_LABEL,
+} from '../constants';
 
 export const addBord = (title) => ({
   type: ADD_BOARD,
@@ -6,96 +26,96 @@ export const addBord = (title) => ({
 });
 
 export const changeNameBoard = (id, text) => ({
-  type: ADD_BOARD,
+  type: CHANGE_NAME_BOARD,
   text,
   id,
 });
 
 export const deleteBord = (id) => ({
-  type: ADD_BOARD,
+  type: DELETE_BOARD,
   id,
 });
 
 export const addCard = (title) => ({
-  type: ADD_BOARD,
+  type: ADD_CARD,
   title,
 });
 
 export const changeNameCard = (id, text) => ({
-  type: ADD_BOARD,
+  type: CHANGE_NAME_CARD,
   text,
   id,
 });
 
 export const changeDescriptionCard = (id, text) => ({
-  type: ADD_BOARD,
+  type: CHANGE_DESCRIPTION_CARD,
   text,
   id,
 });
 
 export const deleteCard = (id) => ({
-  type: ADD_BOARD,
+  type: DELETE_CARD,
   id,
 });
 
 export const addChecklist = (title) => ({
-  type: ADD_BOARD,
+  type: ADD_CHECKLIST,
   title,
 });
 
 export const changeNameChecklist = (id, text) => ({
-  type: ADD_BOARD,
+  type: CHANGE_NAME_CHECKLIST,
   text,
   id,
 });
 
 export const deleteChecklist = (id) => ({
-  type: ADD_BOARD,
+  type: DELETE_CHECKLIST,
   id,
 });
 
 export const addTask = (text) => ({
-  type: ADD_BOARD,
+  type: ADD_TASK,
   text,
 });
 
 export const changeStatusTask = (id) => ({
-  type: ADD_BOARD,
+  type: CHANGEE_STATUS_TASK,
   id,
 });
 
 export const changeNameTask = (id, text) => ({
-  type: ADD_BOARD,
+  type: CHANGEE_NAME_TASK,
   id,
   text,
 });
 
 export const deleteTask = (text) => ({
-  type: ADD_BOARD,
+  type: DELETE_TASK,
   text,
 });
 
 export const addLabelToSearch = (id) => ({
-  type: ADD_BOARD,
+  type: ADD_LABEL_TO_SEARCH,
   id,
 });
 
 export const deleteLabelToSearch = (id) => ({
-  type: ADD_BOARD,
+  type: DELETE_LABEL_TO_SEARCH,
   id,
 });
 
-export const changeTextForSearch = (text) => ({
-  type: ADD_BOARD,
+export const changeTextToSearch = (text) => ({
+  type: CHANGE_TEXT_TO_SEARCH,
   text,
 });
 
 export const createColorLabel = (color) => ({
-  type: ADD_BOARD,
+  type: CREATE_COLOR_LABEL,
   color,
 });
 
 export const deleteColorLabel = (id) => ({
-  type: ADD_BOARD,
+  type: DELETE_COLOR_LABEL,
   id,
 });
